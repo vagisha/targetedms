56469fd9d861314d04584d4a2951f1c550241f6c
Josh Eckels <jeckels@labkey.com>
2013-03-20 16:29:50 +0000

    Add MS1 module to TargetedMS folder type
    SVN r25268 |2013-03-20 16:29:50 +0000

--- src/org/labkey/targetedms/TargetedMSFolderType.java
+++ src/org/labkey/targetedms/TargetedMSFolderType.java
@@ -40,7 +40,7 @@ public class TargetedMSFolderType extends MultiPortalFolderType
                     Portal.getPortalPart(TargetedMSModule.TARGETED_MS_RUNS_WEBPART_NAME).createWebPart()
             ),
             Collections.<Portal.WebPart>emptyList(),
-            getDefaultModuleSet(module, getModule("TargetedMS"), getModule("Pipeline"), getModule("Experiment"), getModule("MS2")),
+            getDefaultModuleSet(module, getModule("TargetedMS"), getModule("Pipeline"), getModule("Experiment"), getModule("MS2"), getModule("MS1")),
             module);
     }
 
