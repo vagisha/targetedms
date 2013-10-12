e5ab3ca02ba22bd8240cdb6fd2109a65451d2c01
Vagisha Sharma <vsharma@uw.edu>
2013-10-12 07:04:41 +0000

    Issue 18325: TargetedMS - IsotopeModification table has duplicate modifications  -- table updates
    SVN r28861 |2013-10-12 07:04:41 +0000

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -104,7 +104,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public double getVersion()
     {
-        return 13.22;
+        return 13.23;
     }
 
     @Override
