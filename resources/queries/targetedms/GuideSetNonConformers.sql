0340abd0a61081f49550e420f2fe28ad7fce7dd5
Cory Nathe <cnathe@labkey.com>
2015-07-22 14:36:10 +0000

    Changes to SQL for readability, no functional change
    SVN r38858 |2015-07-22 14:36:10 +0000

--- resources/queries/targetedms/GuideSetNonConformers.sql
+++ resources/queries/targetedms/GuideSetNonConformers.sql
@@ -51,9 +51,11 @@ UNION SELECT stats.GuideSetId,
 'FWHM' AS Metric,
 'Full Width at Half Maximum (FWHM)' AS MetricLongLabel,
 SUM(CASE WHEN X.Value > (stats.Mean + (3 * stats.StandardDev)) OR X.Value < (stats.Mean - (3 * stats.StandardDev)) THEN 1 ELSE 0 END) AS NonConformers
- FROM (SELECT PrecursorId.ModifiedSequence AS Sequence,
-       PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
-       MaxFWHM AS Value FROM PrecursorChromInfo
+FROM (
+  SELECT PrecursorId.ModifiedSequence AS Sequence,
+  PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
+  MaxFWHM AS Value
+  FROM PrecursorChromInfo
 ) X
 LEFT JOIN GuideSetFWHMStats stats
   ON X.Sequence = stats.Sequence
@@ -66,13 +68,16 @@ UNION SELECT stats.GuideSetId,
 'FWB' As Metric,
 'Full Width at Base (FWB)' As MetricLongLabel,
 SUM(CASE WHEN X.Value > (stats.Mean + (3 * stats.StandardDev)) OR X.Value < (stats.Mean - (3 * stats.StandardDev)) THEN 1 ELSE 0 END) AS NonConformers
- FROM (SELECT PrecursorId.ModifiedSequence AS Sequence,
-       PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
-       PeptideChromInfoId.SampleFileId.FilePath AS FilePath,
-       (MaxEndTime - MinStartTime) AS Value FROM PrecursorChromInfo) X
- LEFT JOIN GuideSetFWBStats stats
-ON X.Sequence = stats.Sequence AND ((X.AcquiredTime >= stats.TrainingStart
-AND X.AcquiredTime < stats.ReferenceEnd) OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
+FROM (
+  SELECT PrecursorId.ModifiedSequence AS Sequence,
+  PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
+  (MaxEndTime - MinStartTime) AS Value
+  FROM PrecursorChromInfo
+) X
+LEFT JOIN GuideSetFWBStats stats
+  ON X.Sequence = stats.Sequence
+  AND ((X.AcquiredTime >= stats.TrainingStart AND X.AcquiredTime < stats.ReferenceEnd)
+    OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
 WHERE stats.GuideSetId IS NOT NULL
 GROUP BY stats.GuideSetId
 
@@ -80,13 +85,16 @@ UNION SELECT stats.GuideSetId,
 'L/H ratio' As Metric,
 'Light/Heavy Ratio' As MetricLongLabel,
 SUM(CASE WHEN X.Value > (stats.Mean + (3 * stats.StandardDev)) OR X.Value < (stats.Mean - (3 * stats.StandardDev)) THEN 1 ELSE 0 END) AS NonConformers
- FROM (SELECT PrecursorChromInfoId.PrecursorId.ModifiedSequence AS Sequence,
-       PrecursorChromInfoId.PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
-       PrecursorChromInfoId.PeptideChromInfoId.SampleFileId.FilePath AS FilePath,
-       AreaRatio AS Value FROM PrecursorAreaRatio) X
- LEFT JOIN GuideSetLHRatioStats stats
-ON X.Sequence = stats.Sequence AND ((X.AcquiredTime >= stats.TrainingStart
-AND X.AcquiredTime < stats.ReferenceEnd) OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
+FROM (
+  SELECT PrecursorChromInfoId.PrecursorId.ModifiedSequence AS Sequence,
+  PrecursorChromInfoId.PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
+  AreaRatio AS Value
+  FROM PrecursorAreaRatio
+) X
+LEFT JOIN GuideSetLHRatioStats stats
+  ON X.Sequence = stats.Sequence
+  AND ((X.AcquiredTime >= stats.TrainingStart AND X.AcquiredTime < stats.ReferenceEnd)
+    OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
 WHERE stats.GuideSetId IS NOT NULL
 GROUP BY stats.GuideSetId
 
@@ -94,12 +102,15 @@ UNION SELECT stats.GuideSetId,
 'T/PA Ratio' As Metric,
 'Transition/Precursor Area Ratio' As MetricLongLabel,
 SUM(CASE WHEN X.Value > (stats.Mean + (3 * stats.StandardDev)) OR X.Value < (stats.Mean - (3 * stats.StandardDev)) THEN 1 ELSE 0 END) AS NonConformers
- FROM (SELECT PrecursorId.ModifiedSequence AS Sequence,
-       PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
-       PeptideChromInfoId.SampleFileId.FilePath AS FilePath,
-       transitionPrecursorRatio AS Value FROM PrecursorChromInfo) X
- LEFT JOIN GuideSetTPRatioStats stats
-ON X.Sequence = stats.Sequence AND ((X.AcquiredTime >= stats.TrainingStart
-AND X.AcquiredTime < stats.ReferenceEnd) OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
+FROM (
+  SELECT PrecursorId.ModifiedSequence AS Sequence,
+  PeptideChromInfoId.SampleFileId.AcquiredTime AS AcquiredTime,
+  transitionPrecursorRatio AS Value
+  FROM PrecursorChromInfo
+) X
+LEFT JOIN GuideSetTPRatioStats stats
+  ON X.Sequence = stats.Sequence
+  AND ((X.AcquiredTime >= stats.TrainingStart AND X.AcquiredTime < stats.ReferenceEnd)
+    OR (X.AcquiredTime >= stats.TrainingStart AND stats.ReferenceEnd IS NULL))
 WHERE stats.GuideSetId IS NOT NULL
 GROUP BY stats.GuideSetId
