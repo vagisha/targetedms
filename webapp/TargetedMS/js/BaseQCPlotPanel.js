e7b62c7273473de04171de2200515f811e8aa965
Adam Rauch <adam@labkey.com>
2015-07-26 20:20:09 +0000

    Update licenses
    SVN r38938 |2015-07-26 20:20:09 +0000

--- webapp/TargetedMS/js/BaseQCPlotPanel.js
+++ webapp/TargetedMS/js/BaseQCPlotPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * Created by binalpatel on 7/22/15.
  */
