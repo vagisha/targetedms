ff446401c9ac15dad160830780c7549d6c26d027
Vagisha Sharma <vsharma@uw.edu>
2015-11-06 22:05:56 +0000

    TargetedMS: Use LabKey SQL for more efficient query for chromatograms table on the peptide details page.
    SVN r40906 |2015-11-06 22:05:56 +0000

--- src/org/labkey/targetedms/query/PeptidePrecursorChromatogramsTableInfo.java
+++ src/org/labkey/targetedms/query/PeptidePrecursorChromatogramsTableInfo.java
@@ -19,11 +19,13 @@ import com.drew.lang.annotations.Nullable;
 import org.apache.commons.lang3.StringUtils;
 import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.SQLFragment;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.query.FilteredTable;
 import org.labkey.api.query.QueryDefinition;
 import org.labkey.api.query.QueryException;
 import org.labkey.api.query.QueryService;
+import org.labkey.api.query.SchemaKey;
 import org.labkey.api.security.User;
 import org.labkey.targetedms.TargetedMSController;
 import org.labkey.targetedms.TargetedMSManager;
@@ -44,12 +46,14 @@ import java.util.List;
  */
 public class PeptidePrecursorChromatogramsTableInfo extends FilteredTable<TargetedMSSchema>
 {
+    private static final String _peptideChromInfoCol = "pepciId";
+
     public PeptidePrecursorChromatogramsTableInfo(TargetedMSSchema schema, Peptide peptide,
                                                   TargetedMSController.ChromatogramForm form)
     {
         super(getPivotByPrecursorChromInfoTable(schema.getContainer(), schema.getUser(), peptide, form.getAnnotationFilter(), form.getReplicatesFilterList()), schema);
         wrapAllColumns(true);
-        ColumnInfo pepChromCol = getColumn("pepciId");
+        ColumnInfo pepChromCol = getColumn(_peptideChromInfoCol);
         pepChromCol.setLabel("");
 
         pepChromCol.setDisplayColumnFactory(new ChromatogramDisplayColumnFactory(
@@ -84,13 +88,13 @@ public class PeptidePrecursorChromatogramsTableInfo extends FilteredTable<Target
 
     private List<ColumnInfo> getPrecursorChromInfoColumns()
     {
-        return getColumns().subList(5, getColumns().size());
+        return getColumns().subList(4, getColumns().size());
     }
 
     public List<String> getDisplayColumnNames()
     {
         List<String> colNames = new ArrayList<>();
-        colNames.add("pepciId");
+        colNames.add(_peptideChromInfoCol);
 		// Sort the precursor chrom info columns
         List<ColumnInfo> colInfoList = new ArrayList<>(getPrecursorChromInfoColumns());
         Collections.sort(colInfoList, new Comparator<ColumnInfo>()
@@ -125,71 +129,52 @@ public class PeptidePrecursorChromatogramsTableInfo extends FilteredTable<Target
 
     private static TableInfo getPivotByPrecursorChromInfoTable(Container container, User user, Peptide peptide,@Nullable List<ReplicateAnnotation> filterAnnotations, @Nullable List<Integer> replicatesFilter)
     {
-
-        String sql =
-            "SELECT replicate, sample, isotopecharge, pepciId, peptideId, MIN(pid) AS preciId FROM \n"+
-            "  (SELECT\n"+
-            "  rep.Name AS replicate,\n"+
-            "  sample.SampleName AS sample,\n" +
-            "  label.Name || CAST(pre.charge AS VARCHAR) AS isotopecharge,\n"+
-            "  pepci.Id AS pepciId,\n" +
-            "  pepci.PeptideId AS peptideId,\n" +
-            "  preci.Id AS pid\n" +
-
-            "FROM\n"+
-            "  " + TargetedMSManager.getTableInfoReplicate() + " rep,\n" +
-            "  " + TargetedMSManager.getTableInfoSampleFile() + " sample,\n"+
-            "  " + TargetedMSManager.getTableInfoIsotopeLabel() + " label,\n"+
-            "  " + TargetedMSManager.getTableInfoPeptideChromInfo() + " pepci,\n"+
-            "  " + TargetedMSManager.getTableInfoPrecursorChromInfo() + "  preci,\n"+
-            "  " + TargetedMSManager.getTableInfoPrecursor() + "  pre\n"+
-            "WHERE\n"+
-            "  rep.Id = sample.ReplicateId\n" +
-            "  AND\n" +
-            "  sample.Id = pepci.SampleFileId\n" +
-            "  AND\n" +
-            "  pepci.Id = preci.PeptideChromInfoId\n" +
-            "  AND\n" +
-            "  pre.Id = preci.PrecursorId\n" +
-            "  AND\n" +
-            "  pre.IsotopeLabelId = label.Id\n" +
-            "  AND\n" +
-            "  pre.PeptideId = "+peptide.getId()+"\n";
-       if(replicatesFilter != null && replicatesFilter.size() != 0)
-       {
-           sql+="AND\n";
-           sql+="( ";
-           String replicateIds = StringUtils.join(replicatesFilter,",");
-           sql+="rep.Id IN ("+replicateIds+")";
-           sql += ")"+"\n";
-       }
+        SQLFragment sql = new SQLFragment("SELECT");
+        sql.append(" replicate, sample,isotopecharge, ").append(_peptideChromInfoCol).append(", MIN(preciId) AS preciId FROM");
+        sql.append(" ( SELECT");
+        sql.append(" SampleFileId.ReplicateId.Name AS replicate");
+        sql.append(", SampleFileId.SampleName AS sample");
+        sql.append(", PeptideChromInfoId AS ").append(_peptideChromInfoCol);
+        sql.append(", (PrecursorId.IsotopeLabelId.Name || CAST(PrecursorId.Charge AS VARCHAR)) AS isotopecharge");
+        sql.append(", Id AS preciId");
+        sql.append(" FROM ");
+        sql.append(TargetedMSManager.getTableInfoPrecursorChromInfo(), "pci");
+        sql.append(" WHERE PrecursorId.PeptideId=").append(peptide.getId());
+        if(replicatesFilter != null && replicatesFilter.size() != 0)
+        {
+            sql.append(" AND ");
+            sql.append("( ");
+            String replicateIds = StringUtils.join(replicatesFilter,",");
+            sql.append("SampleFileId.ReplicateId IN ("+replicateIds+")");
+            sql.append(")");
+        }
         if(filterAnnotations != null)
         {
-            sql += "  AND\n" +
-                    " rep.Id IN (SELECT replicateid FROM " +
-                    TargetedMSManager.getTableInfoReplicateAnnotation()
-                    + " WHERE ";
+            sql.append(" AND ")
+               .append(" SampleFileId.ReplicateId IN (SELECT replicateId FROM ")
+               .append(TargetedMSManager.getTableInfoReplicateAnnotation(), "repAnnot")
+               .append(" WHERE ");
             boolean first = true;
             for(ReplicateAnnotation annotation: filterAnnotations)
             {
                 if(!first)
                 {
-                    sql += " OR ";
+                    sql.append(" OR ");
                 }
-                sql+= " (name = '" + annotation.getName() +"'  " +
-                      "  AND value = '" + annotation.getValue()+"')";
+                sql.append(" (name = '" + annotation.getName() +"'  ")
+                   .append("  AND value = '" + annotation.getValue()+"')");
 
                 first = false;
             }
-            sql += ")"+"\n";
+            sql.append(")");
         }
-        sql += " ) X\n"+
-            "GROUP BY replicate, sample, pepciId, peptideId, isotopecharge\n" +
-            "PIVOT preciId BY isotopecharge";
+        sql.append(" ) X");
+        sql.append(" GROUP BY replicate, sample, ").append(_peptideChromInfoCol).append(", isotopecharge")
+           .append(" PIVOT preciId BY isotopecharge");
 
-        QueryDefinition qdef = QueryService.get().createQueryDef(user, container, TargetedMSSchema.SCHEMA_NAME,
+        QueryDefinition qdef = QueryService.get().createQueryDef(user, container, SchemaKey.fromString(TargetedMSSchema.SCHEMA_NAME),
                                                                  "PeptideChromInfo_pivotByIsotopeLabelCharge");
-        qdef.setSql(sql);
+        qdef.setSql(sql.getSQL());
         qdef.setIsHidden(true);
 
         List<QueryException> errors = new ArrayList<>();
