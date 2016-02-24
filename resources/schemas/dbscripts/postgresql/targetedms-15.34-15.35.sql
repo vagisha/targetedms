4c4795e77dc0d07557997eb4d475d93d511976fd
Binal Patel <binalp@labkey.com>
2016-02-24 01:52:58 +0000

    Small molecule support - added necessary columns to MoleculePrecursor table.
    SVN r42518 |2016-02-24 01:52:58 +0000

---
+++ resources/schemas/dbscripts/postgresql/targetedms-15.34-15.35.sql
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ALTER TABLE targetedms.moleculeprecursor ADD COLUMN IonFormula VARCHAR(100);
+ALTER TABLE targetedms.moleculeprecursor ADD COLUMN CustomIonName VARCHAR(100);
+ALTER TABLE targetedms.moleculeprecursor ADD COLUMN MassMonoisotopic DOUBLE PRECISION NOT NULL;
+ALTER TABLE targetedms.moleculeprecursor ADD COLUMN MassAverage DOUBLE PRECISION NOT NULL;
