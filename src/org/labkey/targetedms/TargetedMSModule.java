5c1efe7719444160ff1645d2ea1297c5f7120be2
Binal Patel <binalp@labkey.com>
2016-02-25 02:36:55 +0000

    Bumping the module version to match the latest script version.
    SVN r42537 |2016-02-25 02:36:55 +0000

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -134,7 +134,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public double getVersion()
     {
-        return 15.35;
+        return 15.36;
     }
 
     @Override
