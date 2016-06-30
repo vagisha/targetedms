39bc967f20c01490b8cf4f1de5b650808303d8d5
Adam Rauch <adam@labkey.com>
2016-06-30 23:37:52 +0000

    Bump several more "managed" modules to 16.20 in preparation for release
    Leaving GEL modules since those are waiting for script merges. Leaving Issues out of an abundance of caution.
    
    SVN r45012 |2016-06-30 23:37:52 +0000

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -143,7 +143,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public double getVersion()
     {
-        return 16.15;
+        return 16.20;
     }
 
     @Override
