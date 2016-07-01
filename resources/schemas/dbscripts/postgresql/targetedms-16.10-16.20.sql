44c56f9829357c82a87f4621c91390932df74901
Adam Rauch <adam@labkey.com>
2016-07-01 14:47:00 +0000

    Consolidate scripts to 16.10-16.20
    SVN r45014 |2016-07-01 14:47:00 +0000

---
+++ resources/schemas/dbscripts/postgresql/targetedms-16.10-16.20.sql
@@ -0,0 +1,31 @@
+/* targetedms-16.10-16.11.sql */
+
+/* The run related count values are now calculated by the server in TargetedMSSchema.getTargetedMSRunsTable */
+ALTER TABLE targetedms.Runs DROP COLUMN PeptideGroupCount;
+ALTER TABLE targetedms.Runs DROP COLUMN PeptideCount;
+ALTER TABLE targetedms.Runs DROP COLUMN PrecursorCount;
+ALTER TABLE targetedms.Runs DROP COLUMN TransitionCount;
+
+/* FK from targetedms.Peptide to targetedms.GeneralMolecule wasn't applied (issue 25789) */
+ALTER TABLE targetedms.Peptide ADD CONSTRAINT FK_Id_GMId FOREIGN KEY (Id) REFERENCES targetedms.GeneralMolecule (Id);
+
+/* targetedms-16.12-16.13.sql */
+
+ALTER TABLE targetedms.Peptide ALTER COLUMN Sequence TYPE VARCHAR(300);
+ALTER TABLE targetedms.Peptide ALTER COLUMN PeptideModifiedSequence TYPE VARCHAR(500);
+ALTER TABLE targetedms.Precursor ALTER COLUMN ModifiedSequence TYPE VARCHAR(2500);
+ALTER TABLE targetedms.iRTPeptide ALTER COLUMN ModifiedSequence TYPE VARCHAR(500);
+
+/* targetedms-16.13-16.14.sql */
+
+ALTER TABLE targetedms.Peptide ALTER COLUMN CalcNeutralMass TYPE DOUBLE PRECISION;
+ALTER TABLE targetedms.Precursor ALTER COLUMN NeutralMass TYPE DOUBLE PRECISION;
+
+/* targetedms-16.14-16.15.sql */
+
+-- Skyline-daily 3.5.1.9426 (and patch release of Skyline 3.5) changed the format of the modified_sequence attribute
+-- of the <precursor> element to always have a decimal place in the modification mass string.
+-- Example: [+80.0] instead of [+80].
+-- Replace strings like [+80] in the modified sequence with [+80.0].
+-- Example: K[+96.2]VN[-17]K[+34.1]TES[+80]K[+62.1] -> K[+96.2]VN[-17.0]K[+34.1]TES[+80.0]K[+62.1]
+UPDATE targetedms.precursor SET ModifiedSequence = (REGEXP_REPLACE(ModifiedSequence, '(\[[+-]\d+)\]', '\1.0]', 'g'));
