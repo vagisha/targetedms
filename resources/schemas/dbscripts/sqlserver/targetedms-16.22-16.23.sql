10741986ea71607e82e59b33321708a826721983
Vagisha Sharma <vsharma@uw.edu>
2016-09-14 16:44:52 +0000

    TargetedMS: - Issue 27789: Need to expand MeasuredIonName column - Added index on runs.experimentrunlsid
    SVN r46363 |2016-09-14 16:44:52 +0000

---
+++ resources/schemas/dbscripts/sqlserver/targetedms-16.22-16.23.sql
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2015-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specifict language governing permissions and
+ * limitations under the License.
+ */
+
+-- ExperimentRunLSID references exp.experimentrun.lsid
+CREATE INDEX IX_Runs_ExperimentRunLSID ON targetedms.Runs(ExperimentRunLSID);
+
+ALTER TABLE targetedms.transition ALTER COLUMN MeasuredIonName NVARCHAR(255);
+
