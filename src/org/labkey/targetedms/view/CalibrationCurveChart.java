22a50559f04ad156e9090de31db12a6d3992b0ea
Marty Pradere <martyp@labkey.com>
2017-03-31 22:11:16 +0000

    Spec 29360: Code cleanup.  Remove JFreeChart implementation of calibration curve.
    SVN r50200 |2017-03-31 22:11:16 +0000

--- src/org/labkey/targetedms/view/CalibrationCurveChart.java
+++ src/org/labkey/targetedms/view/CalibrationCurveChart.java
@@ -16,15 +16,6 @@ package org.labkey.targetedms.view;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.labkey.targetedms.calculations.quantification.CalibrationCurve;
-import org.labkey.targetedms.calculations.quantification.SampleType;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYDotRenderer;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.TableSelector;
 import org.labkey.api.security.User;
@@ -33,6 +24,8 @@ import org.labkey.targetedms.TargetedMSManager;
 import org.labkey.targetedms.TargetedMSRun;
 import org.labkey.targetedms.calculations.ReplicateDataSet;
 import org.labkey.targetedms.calculations.RunQuantifier;
+import org.labkey.targetedms.calculations.quantification.CalibrationCurve;
+import org.labkey.targetedms.calculations.quantification.SampleType;
 import org.labkey.targetedms.parser.CalibrationCurveEntity;
 import org.labkey.targetedms.parser.GeneralMolecule;
 import org.labkey.targetedms.parser.GeneralMoleculeChromInfo;
@@ -42,9 +35,7 @@ import org.labkey.targetedms.query.MoleculeManager;
 import org.labkey.targetedms.query.PeptideManager;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *  Creates a calibration curve chart. In the future, the chart should be displayed alongside controls which allow
@@ -156,65 +147,5 @@ public class CalibrationCurveChart
         json.put("dataPoints", jsonPoints);
 
         return json;
-    }
-
-    public JFreeChart getChart() {
-        TargetedMSRun run = TargetedMSManager.getRun(_form.getId());
-        RunQuantifier runQuantifier = new RunQuantifier(run, _user, _container);
-        CalibrationCurveEntity calibrationCurve = new TableSelector(TargetedMSManager.getTableInfoCalibrationCurve())
-                .getObject(_form.getCalibrationCurveId(), CalibrationCurveEntity.class);
-        QuantificationSettings quantificationSettings = new TableSelector(TargetedMSManager.getTableInfoQuantificationSettings())
-            .getObject(_container, calibrationCurve.getQuantificationSettingsId(), QuantificationSettings.class);
-
-        GeneralMolecule generalMolecule = PeptideManager.getPeptide(_container, calibrationCurve.getGeneralMoleculeId());
-        if (generalMolecule == null) {
-            generalMolecule = MoleculeManager.getMolecule(_container, calibrationCurve.getGeneralMoleculeId());
-        }
-        List<GeneralMoleculeChromInfo> chromInfos = new ArrayList<>();
-        CalibrationCurve recalcedCalibrationCurve
-                = runQuantifier.calculateCalibrationCurve(quantificationSettings, generalMolecule, chromInfos);
-        XYDataset xyDataset = makeXYSeriesCollection(generalMolecule, runQuantifier.getReplicateDataSet(), recalcedCalibrationCurve, chromInfos);
-        XYDotRenderer renderer = new XYDotRenderer();
-        renderer.setDotHeight(5);
-        renderer.setDotWidth(5);
-        for (int iSeries = 0; iSeries < xyDataset.getSeriesCount(); iSeries++) {
-            SampleType sampleType = (SampleType) xyDataset.getSeriesKey(iSeries);
-            renderer.setSeriesPaint(iSeries, sampleType.getColor());
-        }
-        XYPlot xyPlot = new XYPlot(xyDataset, new NumberAxis("Calculated Concentration"), new NumberAxis("Normalized Area"), renderer);
-        JFreeChart chart = new JFreeChart("Calibration Curve: " + generalMolecule.getTextId(), xyPlot);
-        return chart;
-    }
-
-    private XYSeriesCollection makeXYSeriesCollection(GeneralMolecule molecule, ReplicateDataSet replicateDataSet, CalibrationCurve calibrationCurve, Iterable<GeneralMoleculeChromInfo> chromInfos) {
-        Map<SampleType, XYSeries> seriesMap = new HashMap<>();
-        XYSeriesCollection xySeriesCollection = new XYSeriesCollection();
-        for (GeneralMoleculeChromInfo chromInfo : chromInfos) {
-            Replicate replicate = replicateDataSet.getReplicateFromSampleFileId(chromInfo.getSampleFileId());
-            if (replicate == null) {
-                continue;
-            }
-            SampleType sampleType = SampleType.fromName(replicate.getSampleType());
-            if (sampleType == SampleType.blank || sampleType == SampleType.solvent || sampleType == SampleType.double_blank) {
-                continue;
-            }
-            XYSeries xySeries = seriesMap.get(sampleType);
-            if (xySeries == null) {
-                xySeries = new XYSeries(sampleType);
-                xySeriesCollection.addSeries(xySeries);
-                seriesMap.put(sampleType, xySeries);
-            }
-            Double y = calibrationCurve.getY(chromInfo.getCalculatedConcentration());
-            Double x = replicate.getAnalyteConcentration();
-            if (x != null) {
-                if (molecule.getConcentrationMultiplier() != null) {
-                    x *= molecule.getConcentrationMultiplier();
-                }
-            } else {
-                x = chromInfo.getCalculatedConcentration();
-            }
-            xySeries.add(x, y);
-        }
-        return xySeriesCollection;
     }
 }
