c035fcf30e85af616aa584829cc4602677e2f2f5
Trey Chadick <tchad@labkey.com>
2017-04-19 18:13:03 +0000

    29360: Quantitation folder UI Automated test
    SVN r50549 |2017-04-19 18:13:03 +0000

---
+++ test/src/org/labkey/test/components/targetedms/CalibrationCurveWebpart.java
@@ -0,0 +1,97 @@
+package org.labkey.test.components.targetedms;
+
+import org.junit.Assert;
+import org.labkey.test.Locator;
+import org.labkey.test.components.BodyWebPart;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class CalibrationCurveWebpart extends BodyWebPart<CalibrationCurveWebpart.ElementCache>
+{
+    public CalibrationCurveWebpart(WebDriver driver)
+    {
+        super(driver, "Calibration Curve");
+    }
+
+    public List<String> getSvgLegendItems()
+    {
+        final List<String> texts = getWrapper().getTexts(elementCache().findLegendItems());
+        texts.removeAll(Arrays.asList(""));
+        return texts;
+    }
+
+    public List<WebElement> getSvgPoints()
+    {
+        return elementCache().findPoints();
+    }
+
+    public CalibrationCurveWebpart selectAnyPoint()
+    {
+        final List<WebElement> svgPoints = getSvgPoints();
+        if (svgPoints.isEmpty())
+            Assert.fail("SVG has no points to click");
+
+        boolean successful = false;
+        for (WebElement point : svgPoints)
+        {
+            try
+            {
+                point.click();
+                successful = true;
+            }
+            catch (WebDriverException ex)
+            {
+                if (ex.getMessage().contains("Other element would receive the click"))
+                    continue;
+                throw ex;
+            }
+        }
+        if (!successful)
+            Assert.fail("Unable to click any point in calibration curve svg");
+
+        return this;
+    }
+
+    public WebElement getSvg()
+    {
+        elementCache().svg.isSelected(); // trigger findWhenNeeded
+        return elementCache().svg;
+    }
+
+    public File exportToPdf()
+    {
+        getWrapper().mouseOver(elementCache().svg);
+        return getWrapper().doAndWaitForDownload(() -> elementCache().exportToPdf.click());
+    }
+
+    public File exportToPng()
+    {
+        getWrapper().mouseOver(elementCache().svg);
+        return getWrapper().doAndWaitForDownload(() -> elementCache().exportToPng.click());
+    }
+
+    protected ElementCache newElementCache()
+    {
+        return new ElementCache();
+    }
+
+    protected class ElementCache extends BodyWebPart.ElementCache
+    {
+        private final WebElement exportToPng = Locator.id("targetedmsCalibrationCurve-png").findWhenNeeded(this);
+        private final WebElement exportToPdf = Locator.id("targetedmsCalibrationCurve-pdf").findWhenNeeded(this);
+        private final WebElement svg = Locator.css("svg").findWhenNeeded(this);
+        private List<WebElement> findPoints()
+        {
+            return Locator.css("a.point").findElements(svg);
+        }
+        private List<WebElement> findLegendItems()
+        {
+            return Locator.css("g.legend > g.legend-item").findElements(svg);
+        }
+    }
+}
--- test/src/org/labkey/test/tests/targetedms/TargetedMSCalibrationCurveTest.java
+++ test/src/org/labkey/test/tests/targetedms/TargetedMSCalibrationCurveTest.java
@@ -14,6 +14,7 @@
  */
 package org.labkey.test.tests.targetedms;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.labkey.api.reader.TabLoader;
@@ -21,17 +22,21 @@ import org.labkey.test.Locator;
 import org.labkey.test.TestFileUtils;
 import org.labkey.test.categories.DailyB;
 import org.labkey.test.categories.MS2;
+import org.labkey.test.components.targetedms.CalibrationCurveWebpart;
 import org.labkey.test.util.DataRegionTable;
+import org.openqa.selenium.WebElement;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests uploading Skyline documents that contain calibration curve settings. Makes sure that the calculated results
@@ -42,17 +47,31 @@ import static org.junit.Assert.assertNotEquals;
 public class TargetedMSCalibrationCurveTest extends TargetedMSTest
 {
     private static final String SAMPLEDATA_FOLDER = "Quantification/CalibrationScenariosTest/";
-    public static final List<String> scenarioNames = Collections.unmodifiableList(Arrays.asList(
-            "MergedDocuments", "CalibrationTest", "p180test_calibration_DukeApril2016"));
+
+    @BeforeClass
+    public static void setupProject()
+    {
+        TargetedMSCalibrationCurveTest init = (TargetedMSCalibrationCurveTest) getCurrentTest();
+
+        init.setupFolder(FolderType.Experiment);
+    }
 
     @Test
-    public void testCalibrationScenarios() throws Exception
+    public void testMergeDocumentsScenario() throws Exception
     {
-        setupFolder(FolderType.Experiment);
-        for (String scenario : scenarioNames)
-        {
-            runScenario(scenario);
-        }
+        runScenario("MergedDocuments");
+    }
+
+    @Test
+    public void testCalibrationScenario() throws Exception
+    {
+        runScenario("CalibrationTest");
+    }
+
+    @Test
+    public void testP180Scenario() throws Exception
+    {
+        runScenario("p180test_calibration_DukeApril2016");
     }
 
     private void runScenario(String scenario) throws Exception
@@ -91,6 +110,11 @@ public class TargetedMSCalibrationCurveTest extends TargetedMSTest
             clickAndWait(Locator.linkContainingText("calibration curve"));
             waitForText("Calibration Curves");
             DataRegionTable calibrationCurvesTable = new DataRegionTable("calibration_curves" + (smallMolecule ? "_sm_mol" : ""), this);
+            int rowWithData = -1;
+            int rowWithoutData = -1;
+            double expectedSlope = -1;
+            double expectedIntercept = -1;
+            double expectedRSquared = -1;
             for (Map<String, Object> expectedRow : expected)
             {
                 String peptide = expectedRow.get("Peptide").toString();
@@ -102,19 +126,75 @@ public class TargetedMSCalibrationCurveTest extends TargetedMSTest
                 if (expectedErrorMessage != null && expectedErrorMessage.length() > 0)
                 {
                     assertNotEquals("", actualErrorMessage);
+                    rowWithoutData = rowIndex;
                 }
                 else
                 {
                     double actualSlope = Double.parseDouble(calibrationCurvesTable.getDataAsText(rowIndex, "Slope"));
-                    double expectedSlope = Double.parseDouble(expectedRow.get("Slope").toString());
+                    expectedSlope = Double.parseDouble(expectedRow.get("Slope").toString());
                     assertEquals(expectedSlope, actualSlope, getDelta(expectedSlope));
                     double actualIntercept = Double.parseDouble(calibrationCurvesTable.getDataAsText(rowIndex, "Intercept"));
-                    double expectedIntercept = Double.parseDouble(expectedRow.get("Intercept").toString());
+                    expectedIntercept = Double.parseDouble(expectedRow.get("Intercept").toString());
                     assertEquals(expectedIntercept, actualIntercept, getDelta(expectedIntercept));
                     double actualRSquared = Double.parseDouble(calibrationCurvesTable.getDataAsText(rowIndex, "RSquared"));
-                    double expectedRSquared = Double.parseDouble(expectedRow.get("RSquared").toString());
+                    expectedRSquared = Double.parseDouble(expectedRow.get("RSquared").toString());
                     assertEquals(expectedRSquared, actualRSquared, 1E-4);
+                    rowWithData = rowIndex;
                 }
+            }
+
+            log("Verify calibration curve");
+            assertTrue("No data found for calibration curves", rowWithData >= 0);
+            clickAndWait(calibrationCurvesTable.detailsLink(rowWithData));
+            CalibrationCurveWebpart calibrationCurveWebpart = new CalibrationCurveWebpart(getDriver());
+            List<String> actualLegendText = calibrationCurveWebpart.getSvgLegendItems();
+            DecimalFormat df = new DecimalFormat("#.#####");
+            List<String> expectedLegendText = new ArrayList<>(Arrays.asList(
+                    "Standard",
+                    "QC",
+                    "Unknown",
+                    "Calibration Curve",
+                    "Slope: " + df.format(expectedSlope),
+                    "Intercept: " + df.format(expectedIntercept),
+                    "rSquared: " + df.format(expectedRSquared)));
+            assertEquals("Wrong legend text", expectedLegendText, actualLegendText);
+
+            log("Verify calibration curve export");
+            File pdf = calibrationCurveWebpart.exportToPdf();
+            assertTrue("Wrong file type for export pdf [" + pdf.getName() + "]", pdf.getName().endsWith(".pdf"));
+            assertTrue("Empty pdf downloaded [" + pdf.getName() + "]", pdf.length() > 0);
+            File png = calibrationCurveWebpart.exportToPng();
+            assertTrue("Wrong file type for export png [" + png.getName() + "]", png.getName().endsWith(".png"));
+            assertTrue("Empty png downloaded [" + png.getName() + "]", png.length() > 0);
+
+            log("Select a point an re-verify calibration curve");
+            calibrationCurveWebpart.selectAnyPoint();
+            actualLegendText = calibrationCurveWebpart.getSvgLegendItems();
+            assertTrue("Legend didn't update after point selection " + actualLegendText, actualLegendText.contains("Selected Point"));
+            File pdfWithSelection = calibrationCurveWebpart.exportToPdf();
+            assertTrue("Empty pdf export [" + pdfWithSelection.getName() + "]", pdfWithSelection.length() > 0);
+            assertNotEquals("PDF with and without selection shouldn't match", pdf.length(), pdfWithSelection.length());
+            File pngWithSelection = calibrationCurveWebpart.exportToPng();
+            assertTrue("Empty png export [" + pngWithSelection.getName() + "]", pngWithSelection.length() > 0);
+            assertNotEquals("PNG with and without selection shouldn't match", png.length(), pngWithSelection.length());
+
+            if (rowWithoutData >= 0)
+            {
+                goBack();
+                clickAndWait(calibrationCurvesTable.detailsLink(rowWithoutData));
+
+                calibrationCurveWebpart = new CalibrationCurveWebpart(getDriver());
+                assertEquals("Calibration curve with no data shouldn't have any points", 0, calibrationCurveWebpart.getSvgPoints().size());
+                actualLegendText = calibrationCurveWebpart.getSvgLegendItems();
+                expectedLegendText = Arrays.asList(
+                        "Standard",
+                        "QC",
+                        "Unknown",
+                        "Calibration Curve",
+                        "Slope: 0",
+                        "Intercept: 0",
+                        "rSquared: 0");
+                assertEquals("Wrong legend text", expectedLegendText, actualLegendText);
             }
         }
     }
