c1f0dadef95213be9b502bba8e134f9b3cb1c042
Marty Pradere <martyp@labkey.com>
2017-12-22 15:58:16 +0000

    Issue 32457 & 32449: No header info shown for FOM if no data available and js error on export.  Query to get molecule info.
    SVN r55610 |2017-12-22 15:58:16 +0000

---
+++ resources/queries/targetedms/MoleculeInfo.sql
@@ -0,0 +1,16 @@
+SELECT
+  PeptideId,
+  PeptideId.PeptideModifiedSequence as PeptideName,
+  MoleculeId,
+  MoleculeId.CustomIonName as MoleculeName,
+  GROUP_CONCAT(DISTINCT SampleFileId.SampleName, ', ') as SampleFiles,
+	SampleFileId.ReplicateId.RunId.Id as RunId,
+  SampleFileId.ReplicateId.RunId.FileName as FileName
+FROM generalmoleculechrominfo
+GROUP BY
+  PeptideId,
+  PeptideId.PeptideModifiedSequence,
+  MoleculeId,
+  MoleculeId.CustomIonName,
+	SampleFileId.ReplicateId.RunId.Id,
+  SampleFileId.ReplicateId.RunId.FileName
