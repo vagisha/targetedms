7d7e9ec226c3e1698366234924a9e89689f7074d
Ankur Juneja <ankurj@labkey.com>
2019-04-11 23:42:31 +0000

    Item 5206: Query update service so only admins can update configureQCMetric table info
    SVN r63382 |2019-04-11 23:42:31 +0000

--- resources/queries/targetedms/qcMetricsConfig.sql
+++ resources/queries/targetedms/qcMetricsConfig.sql
@@ -2,7 +2,9 @@
 SELECT
        qmc.id,
        qmc.name,
-       ifnull(qem.enabled, 1) AS Enabled
+       ifnull(qem.enabled, 1) AS Enabled,
+       CASE WHEN qem.metric IS NULL THEN FALSE
+            ELSE TRUE END AS Inserted
 FROM
       qcmetricconfiguration qmc
 FULL JOIN   qcenabledmetrics qem
--- src/org/labkey/targetedms/query/QCEnabledMetricsTable.java
+++ src/org/labkey/targetedms/query/QCEnabledMetricsTable.java
@@ -15,10 +15,16 @@
  */
 package org.labkey.targetedms.query;
 
+import org.jetbrains.annotations.NotNull;
 import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.data.ContainerManager;
+import org.labkey.api.query.DefaultQueryUpdateService;
 import org.labkey.api.query.FilteredTable;
+import org.labkey.api.query.QueryUpdateService;
 import org.labkey.api.security.User;
+import org.labkey.api.security.UserPrincipal;
+import org.labkey.api.security.permissions.AdminPermission;
+import org.labkey.api.security.permissions.Permission;
 import org.labkey.targetedms.TargetedMSManager;
 import org.labkey.targetedms.TargetedMSSchema;
 
@@ -43,5 +49,17 @@ public class QCEnabledMetricsTable extends FilteredTable<TargetedMSSchema>
     {
         // the base set of configuration live at the root container
         return new ContainerFilter.CurrentPlusExtras(user, ContainerManager.getRoot());
+    }
+
+    @Override
+    public boolean hasPermission(@NotNull UserPrincipal user, @NotNull Class<? extends Permission> perm)
+    {
+        return getContainer().hasPermission(user, AdminPermission.class);
+    }
+
+    @Override
+    public QueryUpdateService getUpdateService()
+    {
+        return new DefaultQueryUpdateService(this, getRealTable());
     }
 }
