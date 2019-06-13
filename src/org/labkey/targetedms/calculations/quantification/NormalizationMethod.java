7e98928f7caa3ef1a968204320ba98e6547c7fe4
labkey-ankurj <ankurj@labkey.com>
2019-06-13 12:06:17 -0700

    Item 5760: update copyrights

--- src/org/labkey/targetedms/calculations/quantification/NormalizationMethod.java
+++ src/org/labkey/targetedms/calculations/quantification/NormalizationMethod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 LabKey Corporation
+ * Copyright (c) 2017-2019 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may obtain a copy of the License at
