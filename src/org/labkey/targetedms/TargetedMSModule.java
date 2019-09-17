bed65f1eaa86de4c0c9fee1852e84839a11c4e2a
vagisha <vagisha@gmail.com>
2019-09-17 11:35:37 -0700

    Increase size of replicate.name column to 200 (#63)

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -208,7 +208,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public double getVersion()
     {
-        return 19.23;
+        return 19.24;
     }
 
     @Override
