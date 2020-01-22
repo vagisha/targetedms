ec7c9c770e31b7d3ed2aaa4ffa4529835c1fdda8
vagisha <vagisha@gmail.com>
2020-01-21 22:21:49 -0800

    Issue 39477: IllegalFormatConversionException when updating previously imported data in QC folder (#132)

--- src/org/labkey/targetedms/SkylineDocImporter.java
+++ src/org/labkey/targetedms/SkylineDocImporter.java
@@ -484,7 +484,7 @@ public class SkylineDocImporter
             for (SampleFile existingSample : entry.getValue())
             {
                 String srcFile = TargetedMSManager.deleteSampleFileAndDependencies(existingSample.getId());
-                _log.debug(String.format("Updating previously imported data for sample file " + entry.getKey() + " in QC folder. %d of %d", ++s, total));
+                _log.debug(String.format("Updating previously imported data for sample file %s in QC folder. %d of %d", entry.getKey(), ++s, total));
 
                 if (null != srcFile)
                 {
