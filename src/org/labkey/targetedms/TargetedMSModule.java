1919f17b6d4ae848611a0323236e72617b4c94d8
Josh Eckels <jeckels@labkey.com>
2020-01-23 17:31:12 -0800

    Issue 31863: IllegalStateException from org.labkey.targetedms.query.MoleculePrecursorManager.getPrecursorsForMolecule() (#125)
    * Issue 39504: targetedms.AuditLogMessage.messageType column too short

--- src/org/labkey/targetedms/TargetedMSModule.java
+++ src/org/labkey/targetedms/TargetedMSModule.java
@@ -202,7 +202,7 @@ public class TargetedMSModule extends SpringModule implements ProteomicsModule
     @Override
     public double getVersion()
     {
-        return 19.34;
+        return 19.35;
     }
 
     @Override
