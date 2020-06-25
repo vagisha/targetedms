e85fc76d325f170630f2819388e1e7bb46bf906b
Ankur Juneja <ankurj@labkey.com>
2020-06-24 18:10:06 -0700

    Issue 38724: Panorama QC plots can have y-axis label overlaying values (#210)

--- webapp/TargetedMS/js/QCPlotHelperBase.js
+++ webapp/TargetedMS/js/QCPlotHelperBase.js
@@ -629,11 +629,14 @@ Ext4.define("LABKEY.targetedms.QCPlotHelperBase", {
 
         var mainTitle = LABKEY.targetedms.QCPlotHelperWrapper.getQCPlotTypeLabel(plotType, isCUSUMMean);
 
+        var leftMargin = 75;
+        var leftMarginOffset = this.getYAxisLeftMarginOffset(precursorInfo) + leftMargin;
+
         var basePlotConfig = this.getBasePlotConfig(id, precursorInfo.data, plotLegendData);
         var plotConfig = Ext4.apply(basePlotConfig, {
             margins : {
                 top: 65 + this.getMaxStackedAnnotations() * 12,
-                left: 75,
+                left: leftMarginOffset,
                 bottom: 75,
                 right: (this.showInPlotLegends() ? 0 : 30) // if in plot, set to 0 to auto calculate margin; otherwise, set to small value to cut off legend
             },
@@ -647,7 +650,8 @@ Ext4.define("LABKEY.targetedms.QCPlotHelperBase", {
                 },
                 yLeft: {
                     value: this.getYScaleLabel(plotType, trendLineProps.valueConversion, metricProps.series1Label),
-                    color: this.isMultiSeries() ? this.getColorRange()[0] : undefined
+                    color: this.isMultiSeries() ? this.getColorRange()[0] : undefined,
+                    position: leftMarginOffset > 0 ? leftMarginOffset - 15 : undefined
                 },
                 yRight: {
                     value: this.isMultiSeries() ? metricProps.series2Label : undefined,
@@ -688,6 +692,24 @@ Ext4.define("LABKEY.targetedms.QCPlotHelperBase", {
 
         var extraMargin = this.showInPlotLegends() ? 0 : 10 * this.longestLegendText;
         this.attachPlotExportIcons(id, mainTitle + '-' + this.precursors[precursorIndex] + '-' + this.getMetricPropsById(this.metric).series1Label, plotIndex, this.getPlotWidth(), extraMargin);
+    },
+
+    getYAxisLeftMarginOffset: function(precursorInfo)
+    {
+        if (precursorInfo.min === undefined || precursorInfo.max === undefined) {
+            return 0;
+        }
+
+        var maxLength = Math.max(precursorInfo.min.toString().length, precursorInfo.max.toString().length);
+
+        // maxLength of yAxis value
+        // if less than 10 then the current left margin works fine
+        // else add 2 pixels per digit/character
+        if (maxLength < 10) {
+            return 0;
+        } else {
+            return (maxLength - 10) * 2;
+        }
     },
 
     // empty legend to reserve plot space for plot alignment
